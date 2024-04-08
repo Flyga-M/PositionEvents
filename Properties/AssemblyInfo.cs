fca71bd736f5c3ee7438298da7528976138cfa03
Yannick Yannick <yannickjanssen@gmx.de>
2024-04-08 18:37:35 +0200

    AssemblyInfo added, Readme updated

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden 
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("PositionEvents")]
+[assembly: AssemblyDescription("Provides functionality to efficiently bundle position checking (for Blish-Hud modules).")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Flyga-M")]
+[assembly: AssemblyProduct("PositionEvents.Properties")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
+// für COM-Komponenten.  Wenn Sie auf einen Typ in dieser Assembly von 
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("22dc6782-89b5-4a73-b648-2abe01ee13db")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion 
+//      Buildnummer
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # PositionEvents
+Provides functionality to efficiently bundle position checking. Intended for [Blish-Hud](https://github.com/blish-hud/Blish-HUD) modules.
+
+The main `PositionHandler` currently uses an Octree implementation.
+
+## Using PositionEvents with your Blish-Hud module
+> [!WARNING]
+> You should not directly use this library inside your module. Please use the provided Position Events Module as
+> a dependency instead. It's more efficient if multiple modules use the same `PositionHandler`. The module also
+> provides some debug functionality on top.
+
+1. Add the PositionEvents Package as a reference to your module. It is available as a [NuGet](https://www.nuget.org/packages/PositionEvents) package.
+2. Add the Position Events Module as a reference to your module.
+3. Add the Position Events Module as a dependency to your module manifest.
+4. Retrieve a reference to the Position Events Module instance during your `LoadAsync` method.
+5. Register your desired areas with the Position Events Module.
+
+> [!TIP]
+> Make sure that the references of the PositionEvents Package and the Position Events Module have **Copy Local** set
+> to **False** in the properties of the reference. You don't need to ship them with your module, since they will
+> already be present, because of your modules dependence on the Position Events Module.
